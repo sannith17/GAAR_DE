4c9c62d1fc583ed625dfb6aff5699cb7b2447079
V Sannith Kumar <sannith17v@gmail.com>
2026-02-15 21:45:00 +0100

    Create CartIcon.js

---
+++ components/cart/CartIcon.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import { FaShoppingCart } from 'react-icons/fa'
+import { useCart } from '@/context/CartContext'
+
+export default function CartIcon() {
+  const { cart } = useCart()
+  const itemCount = cart.items.reduce((sum, item) => sum + item.quantity, 0)
+
+  return (
+    <Link href="/cart" className="relative text-gray-600 hover:text-primary transition-colors">
+      <FaShoppingCart size={20} />
+      {itemCount > 0 && (
+        <span className="absolute -top-2 -right-2 bg-primary text-white text-xs 
+                       w-5 h-5 rounded-full flex items-center justify-center">
+          {itemCount}
+        </span>
+      )}
+    </Link>
+  )
+}
