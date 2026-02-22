b36d9751895a929fc785467a225835155af723ea
V Sannith Kumar <sannith17v@gmail.com>
2026-02-22 12:44:12 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,28 @@
 import '../styles/globals.css'
 import { CartProvider } from '../context/CartContext'
 import Layout from '../components/layout/Layout'
+import { useState, createContext } from 'react'
+import EdgeLighting from '../components/ui/EdgeLighting'
+
+export const EdgeLightContext = createContext()
 
 export default function App({ Component, pageProps }) {
+  const [edgeLightTrigger, setEdgeLightTrigger] = useState(false)
+  const [edgeLightColor, setEdgeLightColor] = useState('#004aad')
+
   return (
-    <CartProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </CartProvider>
+    <EdgeLightContext.Provider value={{ 
+      edgeLightTrigger, 
+      setEdgeLightTrigger, 
+      edgeLightColor, 
+      setEdgeLightColor 
+    }}>
+      <CartProvider>
+        <Layout>
+          <EdgeLighting trigger={edgeLightTrigger} color={edgeLightColor} />
+          <Component {...pageProps} />
+        </Layout>
+      </CartProvider>
+    </EdgeLightContext.Provider>
   )
 }
