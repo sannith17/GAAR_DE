4a6a1f62022520dd68cd59425e3b14c2878b568e
V Sannith Kumar <sannith17v@gmail.com>
2026-02-22 14:56:37 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,29 @@
 import '../styles/globals.css'
 import { CartProvider } from '../context/CartContext'
 import Layout from '../components/layout/Layout'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // Handle client-side navigation issues
+    const handleRouteChange = (url) => {
+      console.log('Route changed to:', url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router])
+
   return (
     <CartProvider>
       <Layout>
-        <Component {...pageProps} />
+        <Component {...pageProps} key={router.asPath} />
       </Layout>
     </CartProvider>
   )
