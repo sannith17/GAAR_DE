ef8f523e1707cdaf44c57d09545e1e2c4a24f005
V Sannith Kumar <sannith17v@gmail.com>
2026-02-22 14:34:21 +0100

    Update brandLogos.js

--- utils/brandLogos.js
+++ utils/brandLogos.js
@@ -2,9 +2,9 @@
 export const brandLogos = {
   'Audi': 'https://1000logos.net/wp-content/uploads/2018/05/Audi-logo.png',
   'BMW': 'https://1000logos.net/wp-content/uploads/2018/02/BMW-Logo.png',
-  'Mercedes': 'https://1000logos.net/wp-content/uploads/2018/02/Mercedes-Benz-Logo.png',
+  'Mercedes': 'https://i.pinimg.com/originals/7a/7c/32/7a7c32a32c0d68f63168dcfa2e45ad60.png',
   'Porsche': 'https://1000logos.net/wp-content/uploads/2018/02/Porsche-Logo.png',
-  'Volvo': 'https://1000logos.net/wp-content/uploads/2018/02/Volvo-Logo.png'
+  'Volvo': 'https://logos-world.net/wp-content/uploads/2020/04/Volvo-Logo-1971-present.png'
 }
 
 // Fallback color for brands without logos
