d27f9ea9f5c17b0016124e3680557d5be5d9c88e
V Sannith Kumar <sannith17v@gmail.com>
2026-02-22 14:56:12 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ['images.unsplash.com', 'toppng.com', 'premiumwheels.de', 'images-porsche.imgix.net', '1000logos.net', 'i.pinimg.com', 'logos-world.net'],
   },
+  trailingSlash: false,
 }
 
 module.exports = nextConfig
