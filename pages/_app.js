29fc2adc56ff2ec464f23a3d588e712122d34d1d
V Sannith Kumar <sannith17v@gmail.com>
2026-02-22 13:00:29 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,13 @@
 import '../styles/globals.css'
 import { CartProvider } from '../context/CartContext'
-import { EdgeLightProvider } from '../context/EdgeLightContext'
 import Layout from '../components/layout/Layout'
-import EdgeLighting from '../components/ui/EdgeLighting'
 
 export default function App({ Component, pageProps }) {
   return (
-    <EdgeLightProvider>
-      <CartProvider>
-        <Layout>
-          <EdgeLighting />
-          <Component {...pageProps} />
-        </Layout>
-      </CartProvider>
-    </EdgeLightProvider>
+    <CartProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CartProvider>
   )
 }
